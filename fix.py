9ce82afdf47d09b2a0301d653695721ac04ed889
nyk5473 <nyk5473@github.com>
2026-09-03 13:21:03 +0900

    Fix JS SyntaxError caused by unescaped newline in alert

---
+++ fix.py
@@ -0,0 +1,9 @@
+﻿import os, glob
+for fp in glob.glob('*.html'):
+    with open(fp, 'r', encoding='utf-8') as f:
+        c = f.read()
+    if '백업되었습니다!\n(구글' in c:
+        c = c.replace('백업되었습니다!\n(구글', '백업되었습니다!\\n(구글')
+        with open(fp, 'w', encoding='utf-8') as f:
+            f.write(c)
+        print(f'Fixed {fp}')
